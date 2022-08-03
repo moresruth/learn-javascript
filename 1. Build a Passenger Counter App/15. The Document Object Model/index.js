465b19fdb0ecc56b7729d8023b58632772953e3a
moresruth <moresruthantonette.com>
2022-08-03 23:04:14 +0800

    DOM

--- 1. Build a Passenger Counter App/15. The Document Object Model/index.js
+++ 1. Build a Passenger Counter App/15. The Document Object Model/index.js
@@ -1,3 +1,4 @@
+// DOM Document Object Model (how you use JavaScript to modify a website)
 let countEl = document.getElementById("count-el")
 let count = 0
 
--- 1. Build a Passenger Counter App/16. Display the count with innerText/index.js
+++ 1. Build a Passenger Counter App/16. Display the count with innerText/index.js
@@ -3,7 +3,7 @@ let count = 0
 
 function increment() {
     count = count + 1
-    // set countEl's innerText to the count
+    countEl.innerText = count
 }
 
 
