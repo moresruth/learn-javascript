7f465dec80d46d2d3fb1e67e70a7043ceadd3f60
moresruth <moresruthantonette.com>
2022-08-03 22:12:30 +0800

    reassign and increment

--- 1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
+++ 1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
@@ -1,10 +1,19 @@
 // document.getElementById("count").innerText = 5
 
-let count = 5
-
-console.log(count)
+// let count = 5
+// count + 1 (increment)
+// count= count + 12
+// console.log(count)
 
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+let bonusPoints = 50
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
 // Decrease it down to 25, and then finally increase it to 70
 
-
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
